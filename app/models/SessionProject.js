10395bdb40b383410e8c4516159ce7dbb75e4828
Maiko <maikossmaster@gmail.com>
2018-10-05 10:48:45 -0300

    ...

---
+++ app/models/SessionProject.js
@@ -0,0 +1,12 @@
+module.exports = (sequelize, DataTypes) => {
+  const SessionProject = sequelize.define('SessionProject', {
+    title: DataTypes.STRING,
+    content: DataTypes.TEXT,
+  });
+
+  SessionProject.associate = (models) => {
+    SessionProject.belongsTo(models.Project);
+  };
+
+  return SessionProject;
+};
